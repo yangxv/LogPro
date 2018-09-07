7d79c185d033dbb1b73f2337406e4295f06b05d0
yangxu <yangxu@humming-tech.com>
2018-09-07 15:59:14 +0800

    idea 项目整合测试

--- log4jDemo/src/main/java/com/nstc/App.java
+++ log4jDemo/src/main/java/com/nstc/App.java
@@ -6,6 +6,11 @@ import org.slf4j.LoggerFactory;
 public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args ) {
-        logger.info("info");
+        try{
+            System.out.println(5/0);
+        }catch(Exception e){
+            throw e;
+        }
+
     }
 }
